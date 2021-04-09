2bbf6edad702149e297d4a836a111a7f92a655f6
Franklin Waller <fr.l.waller@gmail.com>
2021-04-09 10:39:02 +0200

    feat: Remove Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,6 @@ import { Route, Switch, useHistory } from 'react-router';
 import { routes } from './routes';
 
 function App(): ReactElement {
-    const history = useHistory();
-
-    history.listen((currentLocation) => {
-        // @ts-ignore
-        if (window && window.gtag) {
-            // @ts-ignore
-            window.gtag('config', process.env.REACT_APP_GA_ID, {
-                'page_path': currentLocation.pathname + currentLocation.search,
-            });
-        }
-    });
-
     return (
         <Switch>
             {routes.map((route): ReactElement => <Route {...route} key={route.key} />)}
